83f750592be32ff0a378e7d898c8e716721ace8d
september <zzzhen1994@gmail.com>
2022-02-01 17:10:19 +0800

    fix: lint and test error

--- type_tests/map_key_test.go
+++ type_tests/map_key_test.go
@@ -1,7 +1,3 @@
-// +build go1.15
-// remove these tests temporarily until https://github.com/golang/go/issues/38105 and
-// https://github.com/golang/go/issues/38940 is fixed
-
 package test
 
 import (
